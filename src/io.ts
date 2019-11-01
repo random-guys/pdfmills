1df5930c0fb4f9cca32ed7868ede3a1a42da581a
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-11-01 14:29:18 +0100

    feat: How does one establish CSS style margins on a PDFKit document

--- src/io.ts
+++ src/io.ts
@@ -1,5 +1,10 @@
 import { createWriteStream } from 'fs';
 
+/**
+ * Save a pdf document.
+ * @param pdfKit PDFKit instance
+ * @param path path to store the file
+ */
 export function save(pdfKit: PDFKit.PDFDocument, path: string) {
   pdfKit.pipe(createWriteStream(path));
   pdfKit.end();
