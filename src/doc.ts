b742094c130ec24410b33a01f5b3d8faa290b7ba
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-11-02 12:56:38 +0100

    feat: How do you track the origin y while drawing block elements

--- src/doc.ts
+++ src/doc.ts
@@ -1,12 +1,12 @@
 import PDFDocument from 'pdfkit';
-import { BlockFactory } from './block';
+import { BlockFactory, Block } from './block';
 import { Dimensions, Origins } from './data';
 import { CSSMargins, Margins, toEnglish } from './margin';
 
 export class Document {
   readonly doc: PDFKit.PDFDocument;
   readonly margins: Margins;
-  private origins: Origins = { x: 0, y: 0 };
+  private origins: Origins;
   private dim: Dimensions = {
     width: 595,
     height: 842
@@ -22,9 +22,26 @@ export class Document {
       size: 'A4',
       margins: this.margins
     });
+    this.origins = {
+      x: this.margins.left,
+      y: this.margins.top
+    };
   }
 
+  /**
+   * Use this to draw a set of elements that are arranged vertically, with
+   * each one on a `new line`
+   * @param factory is the function user's will use to describe the block
+   */
   block(factory: BlockFactory) {
-    factory(this.doc, this.origins, this.dim);
+    const block = new Block();
+
+    // pretend draw stuff
+    factory(block);
+
+    // update doc's trackers
+    const height = block.draws(this.doc, this.origins, this.dim);
+    this.origins.y += height;
+    this.dim.height -= height;
   }
 }
