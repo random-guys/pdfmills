97c56163d34f12e0157dac114f3f4f14f16db826
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-11-02 16:55:55 +0100

    feat: how do we add spacing between elements in a block

--- src/block.ts
+++ src/block.ts
@@ -3,16 +3,36 @@ import { Origins, Dimensions } from './data';
 export class Block {
   private elements: BlockElement[] = [];
 
+  /**
+   * Adds a text element to the block. If the text can't take the
+   * width of the block, it'll wrap.
+   * @param text text to add
+   */
   text(text: string) {
     this.elements.push(new TextBlock(text));
   }
 
-  draw(pdfKit: PDFKit.PDFDocument, origins: Origins, dim: Dimensions) {
+  /**
+   * Add some vertical space before the next element
+   * @param size size of space in pixels
+   */
+  space(size: number) {
+    this.elements.push(new SpaceBlock(size));
+  }
+
+  /**
+   * Draw a block while controlling the vertical origin so elements are
+   * on new lines
+   * @param doc PDFKit Document
+   * @param origins x and y
+   * @param dim width and height
+   */
+  draw(doc: PDFKit.PDFDocument, origins: Origins, dim: Dimensions) {
     return this.elements.reduce((height, element) => {
       return (
         height +
         element.draw(
-          pdfKit,
+          doc,
           {
             x: origins.x,
             y: origins.y + height
@@ -34,11 +54,11 @@ export interface BlockElement {
   /**
    * Implementations are expected to use PDFKit and return the height of the
    * element they have just drawn
-   * @param pdfKit PDFKit Document
+   * @param doc PDFKit Document
    * @param origins x and y
    * @param dim width and height
    */
-  draw(pdfKit: PDFKit.PDFDocument, origins: Origins, dim: Dimensions): number;
+  draw(doc: PDFKit.PDFDocument, origins: Origins, dim: Dimensions): number;
 }
 
 export interface BlockFactory {
@@ -48,13 +68,16 @@ export interface BlockFactory {
   (block: Block): void;
 }
 
+/**
+ * Text that can be drawn on a block
+ */
 export class TextBlock implements BlockElement {
   constructor(private text: string) {}
 
   /**
    * Draw `text` within the confines of `dim` and return the height
    * of the drawn text
-   * @param pdfKit PDFKit Document
+   * @param doc PDFKit Document
    * @param origins x and y
    * @param dim width and height
    */
@@ -64,5 +87,23 @@ export class TextBlock implements BlockElement {
       height: dim.height
     });
     return doc.heightOfString(this.text);
+  }
+}
+
+/**
+ * Simple implementation that doesn't draw anything, only add space to the
+ * block
+ */
+export class SpaceBlock implements BlockElement {
+  constructor(private space: number) {}
+
+  /**
+   *
+   * @param doc PDFKit Document
+   * @param origins x and y
+   * @param dim width and height
+   */
+  draw(_doc: PDFKit.PDFDocument, origins: Origins, dim: Dimensions) {
+    return this.space;
   }
 }
