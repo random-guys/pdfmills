ad3e60eb4644f747a3c5725eafc777d5ceb5685f
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-12-08 21:17:14 +0100

    ref: draw implementations in pdf.ts are redundant

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,38 +1,21 @@
-import sumBy from 'lodash/sumBy';
-import { BoundingBox, Context, Element, pageBounds } from './base';
-import { Background, Block, SpaceBetween } from './drawables';
+import { BoundingBox, Context, Element, pageBounds } from "./base";
+import { Background, Block, SpaceBetween, WeightedColumn } from "./drawables";
 
 export function block(context: Context, elements: Element[]) {
   const block = new Block(elements);
-  const boxes = block.boxes(context, context.bounds());
-
-  elements.forEach((el, i) => {
-    el.draw(context, boxes[i]);
-  });
-
-  const fullHeight = sumBy(boxes, b => b.height);
-  context.reframe(0, fullHeight);
-
+  block.draw(context, context.bounds());
   return block;
 }
 
 export function spaceBetween(context: Context, elements: Element[]) {
   const flex = new SpaceBetween(elements);
-  const boxes = flex.boxes(context, context.bounds());
-
-  elements.forEach((el, i) => {
-    el.draw(context, boxes[i]);
-  });
-
-  context.reframe(0, boxes[0].height);
-
+  flex.draw(context, context.bounds());
   return flex;
 }
 
 export function background(context: Context, box?: BoundingBox) {
-  box = box || pageBounds();
   const bg = new Background(251);
-  bg.draw(context, box);
+  bg.draw(context, box || pageBounds());
 
   return bg;
 }
