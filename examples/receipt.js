80c703fa2c365a131b105ab8d150efb9e077ded3
Olakunle Arewa <arewa.olakunle@gmail.com>
2019-12-08 18:01:08 +0100

    chore: how do contributors run the example

--- examples/receipt.js
+++ examples/receipt.js
@@ -1,13 +1,13 @@
-var { Context, pageBounds } = require('../dist/base');
-var { Image, LineBreak, Text, Block } = require('../dist/drawables');
-var { background, block, spaceBetween } = require('../dist/pdf');
-var { save } = require('../dist/utils');
-var path = require('path');
+var { Context, pageBounds } = require("../dist/base");
+var { Image, LineBreak, Text, Block } = require("../dist/drawables");
+var { background, block, spaceBetween } = require("../dist/pdf");
+var { save } = require("../dist/utils");
+var path = require("path");
 
-const assets = path.join(__dirname, 'assets');
-const openSans = path.join(assets, 'fonts/OpenSans-Regular.ttf');
-const openSansSemibold = path.join(assets, 'fonts/OpenSans-Semibold.ttf');
-const goMoneyLogo = path.join(assets, 'img/logo.png');
+const assets = path.join(__dirname, "assets");
+const openSans = path.join(assets, "fonts/OpenSans-Regular.ttf");
+const openSansSemibold = path.join(assets, "fonts/OpenSans-Semibold.ttf");
+const goMoneyLogo = path.join(assets, "img/logo.png");
 
 const context = new Context(54, {
   font: openSans,
@@ -23,7 +23,7 @@ background(context, {
 block(context, [
   new Image(goMoneyLogo, 52, 52),
   new LineBreak(14),
-  new Text('Transaction Statement', {
+  new Text("Transaction Statement", {
     font: openSansSemibold,
     fontSize: 22
   }),
@@ -31,8 +31,9 @@ block(context, [
 ]);
 
 spaceBetween(context, [
-  new Block([new Text('Welcome'), new LineBreak(10), new Text('Home')]),
-  new Block([new Text('Welcome'), new LineBreak(10), new Text('Home')])
+  new Block([new Text("A(1,1)"), new LineBreak(10), new Text("A(1,2)")]),
+  new Block([new Text("A(2,1)"), new LineBreak(10), new Text("A(2,2)")]),
+  new Block([new Text("A(3,1)"), new LineBreak(10), new Text("A(3,2)")])
 ]);
 
-save(context.raw, 'output.pdf');
+save(context.raw, "output.pdf");
