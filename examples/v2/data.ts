9cf873348e56c2d5f87fd236e06e1fee4ce4be9e
Raymond Tukpe <rtukpe@gmail.com>
2020-12-30 18:30:28 +0100

    feat: FlexItem widths were not assigned ahead of time.

--- examples/v2/data.ts
+++ examples/v2/data.ts
@@ -1,10 +1,12 @@
 import faker from "faker";
 import subDays from "date-fns/subDays";
 
+type TransType = "debit" | "credit";
+
 export interface Transaction {
   balance: number;
   date: Date;
-  transaction_type: "debit" | "credit";
+  transaction_type: TransType;
   amount: number;
   remark: string;
   reference?: string;
@@ -16,7 +18,7 @@ function generateTransaction(
   previousTrx: Transaction,
   date: Date
 ): Transaction {
-  const trxType = faker.random.arrayElement(["debit", "credit"]);
+  const trxType: TransType = faker.random.arrayElement(["debit", "credit"]);
   const amount = parseFloat(faker.finance.amount(10_000, 20_000_000, 0));
   const newBalance =
     previousTrx.balance + (trxType === "credit" ? amount : -amount);
@@ -24,28 +26,28 @@ function generateTransaction(
     amount,
     balance: newBalance,
     date: date,
-    remark: faker.lorem.sentence(),
+    remark:
+      "gomoney is authorised by the Central Bank of Nigeria and all deposits are insured by the Nigerian Deposit Insurance Company, gomoney is authorised by the Central Bank of Nigeria and all deposits are insured by the Nigerian Deposit Insurance Company",
     reference: faker.random.uuid(),
     nuban: previousTrx.nuban,
     customer: previousTrx.customer,
-    // @ts-ignore
     transaction_type: trxType
   };
 }
 
 function generateInitialTransaction(account: any, date: Date): Transaction {
-  const trxType = faker.random.arrayElement(["debit", "credit"]);
+  const trxType: TransType = faker.random.arrayElement(["debit", "credit"]);
   const balance = parseFloat(faker.finance.amount(100_000_000, 500_000_000));
   const amount = parseFloat(faker.finance.amount(10_000, 20_000_000, 0));
   return {
     amount,
     balance,
     date,
-    remark: faker.lorem.sentence(),
+    remark:
+      "gomoney is authorised by the Central Bank of Nigeria and all deposits are insured by the Nigerian Deposit Insurance Company, gomoney is authorised by the Central Bank of Nigeria and all deposits are insured by the Nigerian Deposit Insurance Company",
     reference: faker.random.uuid(),
     nuban: account.account_number,
     customer: account.customer_id,
-    // @ts-ignore
     transaction_type: trxType
   };
 }
@@ -54,9 +56,9 @@ export function generateTransactions(account: any) {
   const from = new Date();
   const allTransactions: Transaction[] = [];
 
-  for (let i = 0; i < 7; i++) {
-    const numTrx = faker.random.number({ min: 1, max: 10 });
-    const to = subDays(from, i);
+  for (let i = 0; i < 1; i++) {
+    const numTrx = faker.random.number({ max: 2 });
+    const to = subDays(from, 1);
     const first = generateInitialTransaction(account, to);
 
     const transactions = Array.from({ length: numTrx }).map(() =>
